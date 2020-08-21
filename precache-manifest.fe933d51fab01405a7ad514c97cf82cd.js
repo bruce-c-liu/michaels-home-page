384944566fd53722a25faefc8dcd751b0b5b4391
bruce-c-liu <bruceliuismyname@gmail.com>
2020-08-21 12:52:56 -0700

    Updates

---
+++ precache-manifest.fe933d51fab01405a7ad514c97cf82cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d38d3d52d152f71a130aafc2efd95b",
+    "url": "/michaels-home-page/index.html"
+  },
+  {
+    "revision": "9839e747c18f80e42fe0",
+    "url": "/michaels-home-page/static/css/main.d5ab399e.chunk.css"
+  },
+  {
+    "revision": "4477f916192da5679b47",
+    "url": "/michaels-home-page/static/js/2.e387e6bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/michaels-home-page/static/js/2.e387e6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9839e747c18f80e42fe0",
+    "url": "/michaels-home-page/static/js/main.0aad2292.chunk.js"
+  },
+  {
+    "revision": "f8b5cac0bd8f7d008ddd",
+    "url": "/michaels-home-page/static/js/runtime-main.c1bfc2ad.js"
+  },
+  {
+    "revision": "3f6924518e12bdb1b1581449ba7da5f1",
+    "url": "/michaels-home-page/static/media/cart.3f692451.svg"
+  },
+  {
+    "revision": "0ebf301965b725fa0a1befad8d2a6ae5",
+    "url": "/michaels-home-page/static/media/hero3.0ebf3019.jpg"
+  },
+  {
+    "revision": "49258e62ca7d7e698476c0663f26b640",
+    "url": "/michaels-home-page/static/media/logo.49258e62.svg"
+  },
+  {
+    "revision": "eb492e1b15cd3104c4afb211a36b2a73",
+    "url": "/michaels-home-page/static/media/search.eb492e1b.svg"
+  }
+]);
